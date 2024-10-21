a8e847e923b4da0946b66b375bae771663ba7a7e
smartiri <rasa7825@gmail.com>
2024-10-21 11:10:02 +0200

    createbrowserrouter

--- src/index.js
+++ src/index.js
@@ -5,18 +5,44 @@ import reportWebVitals from "./reportWebVitals";
 import App from "./App.js";
 import Contact from "./contact/Contact.js";
 import Hero from "./hero/Hero.js";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+  useParams,
+} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+{
+  /* kur ke children brenda mos e perdor elementin tek parent */
+}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      {
+        path: "/test",
+        element: <h1>Hello there</h1>,
+      },
+      {
+        path: "/test/:id",
+        element: <h1>Hello there id person</h1>,
+      },
+    ],
+  },
+]);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
+    {/* <BrowserRouter> */}
+    {/* <Routes>
         <Route path="/" element={<App />} />{" "}
         {/* other attributes: errorElement loader */}
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/hero/:slug" element={<Hero />} />
-      </Routes>
-    </BrowserRouter>
+    {/* <Route path="/contact" element={<Contact />} /> */}
+    {/* <Route path="/hero/:slug" element={<Hero />} /> */}
+    {/* </Routes> */}
+    <RouterProvider router={router} />
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
 
